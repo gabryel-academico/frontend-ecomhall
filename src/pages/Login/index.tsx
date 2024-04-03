2117302f4403883070d2937c8690020541001c2a
unknown <Gabryel>
2024-04-03 04:34:09 -0300

    tela de registro cliente

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,25 +7,37 @@ function Login() {
         <div className='container'>
         
         <div className='loginFormulario'>
-            
+                <div id='cabecalhoLogin'>
                 <img src={logo2} alt="logo" />
                 <h2>Faça seu Login</h2>
+                </div>
                 
-                    <TextField id="outlined-basic" label="Email" variant="outlined" />
-                    <TextField  id="outlined-basic" label="Senha" variant="outlined" sx={{
-                        marginBottom: '1rem',
-                        marginTop: '1rem',
-                    }}/>
+                
+                    <div>
+                        <form action="">
+                            <TextField id="outlined-basic" label="Email" variant="outlined" size='small' margin='dense' sx={{
+                                width: "100%",
+                            }}/>
+                            <TextField  id="outlined-basic" label="Senha" variant="outlined" size='small' margin='dense' sx={{
+                                width: "100%",
+                            }}/>
+                        </form>
+                    </div>
                     <Button variant="contained" color='inherit' href="#contained-buttons" sx={{
-                        marginBottom: '1rem',
+                        width: "100%",
                     }}>
                     Realizar Login
                     </Button>
                 
-                    <h4>Ainda não possui uma conta?</h4>
+                    <h4>Ainda não possui uma conta? Crie agora.</h4>
+                    
                     <div className="registro">
-                    <Button variant="contained" color='inherit' href='/registro/cliente'>Cliente</Button>
-                    <Button variant="contained" color='inherit' href='/registro/varejista'>Varejista</Button>
+                    <Button variant="contained" color='inherit' href='/registro/cliente' size='small' sx={{
+                      width: "40%",
+                    }}>Cliente</Button>
+                    <Button variant="contained" color='inherit' href='/registro/varejista' size="small" sx={{
+                        width: "40%",
+                    }}>Varejista</Button>
                     </div>
                 </div>
                 
--- src/pages/Registro/Cliente/index.tsx
+++ src/pages/Registro/Cliente/index.tsx
@@ -1,10 +1,50 @@
 
+import { TextField, Input, Button } from "@mui/material";
+
 import "./style.css"
+import logo2 from "../../../assets/logo2.png"
+
+function handleClick(){
+    alert("TESTE")
+}
 
 function RegCliente(){
     return(
-        <div className="container-regv">
-            <h3>Faça seu cadastro!</h3>
+        <div className="container-reg">
+           <div className="card-reg">
+           <div id="cabecalhoReg">
+               <img src={logo2} alt="logo" />
+               <h2>Faça seu registro!</h2>
+           </div>
+           <div id="form-reg">
+            
+               <form action="">
+                   <TextField id="outlined-basic" label="Nome" variant="outlined" margin="dense" size="small" sx={{
+                    width: "100%",
+                   }}/>
+                   <TextField id="outlined-basic" label="CPF" variant="outlined" margin="dense" size="small" sx={{
+                    width: "100%",
+                   }}/>
+                   <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" size="small" sx={{
+                    width: "100%",
+                   }}/>
+                   <TextField id="outlined-basic" label="Senha" variant="outlined" margin="dense" size="small" sx={{
+                    width: "100%",
+                   }}/>
+                   
+               </form>
+               
+           </div>
+           <Button variant="contained" color='inherit' href="#contained-buttons" sx={{
+            width: "100%",
+           }} onClick={handleClick}>
+           Realizar Cadastro
+           </Button>
+            
+                
+                
+               
+            </div>
 
         </div>
     )
