795f4e2cf7ef7d4a6f4eee8767d8406be23a821d
Lucas Santana <lucas1.stoly@gmail.com>
2024-04-07 15:11:39 -0300

    cadastro de varejista

--- src/pages/Registro/Varejista/index.tsx
+++ src/pages/Registro/Varejista/index.tsx
@@ -2,14 +2,80 @@ import { Button, TextField } from "@mui/material";
 
 import logo2 from "../../../assets/logo2.png"
 import "../style.css"
+import { useState } from "react";
+import { CredenciaisService, ICredenciais } from "../../../shared/Services/api/Registro/CredenciaisService";
+import { IVarejista, VarejistaService } from "../../../shared/Services/api/Registro/Varejista/VarejistaService";
 
 
-function handleClick(){
-    alert("TESTE")
-}
+function RegVarejista()
+{
+    const [nome, setNome] = useState("");
+    const [cnpj, setCnpj] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
 
+    const onSubimit = (event : React.FormEvent<HTMLFormElement>) =>
+    {
+        event.preventDefault();
+        console.log(cnpj)
+        console.log(cnpj.length)
+        if(cnpj.length !== 14 )
+        {
+            console.log("Digite um cnpj válido!");
+            return;
+        }
+        
+        if(nome === "" || email === "" || senha === "")
+        {
+            console.log("Preencha todos os campos!");
+            return;
+        }
 
-function RegVarejista(){
+        const credenciais : ICredenciais = 
+        {
+            email,
+            senha,
+            nome
+        }
+
+        CredenciaisService.create(credenciais).then((result) => 
+        {
+            if(result instanceof Error)
+            {
+                console.error(result.message);
+                return;
+            }
+
+            const varejista : IVarejista = 
+            {
+                cnpj,
+                idCredenciais: result
+            }
+
+            VarejistaService.create(varejista).then((result) =>
+            {
+                if(result instanceof Error)
+                {
+                    console.error(result.message);
+                    return;
+                }
+                limparCampos();
+                console.log("Cadastrado com sucesso!");
+            })
+            
+        })
+
+        
+    }
+
+    const limparCampos = () =>
+    {
+        setNome("");
+        setCnpj("");
+        setEmail("");
+        setSenha("");
+    }
+
     return(
         <div className="container-reg">
            <div className="card-reg">
@@ -19,31 +85,32 @@ function RegVarejista(){
            </div>
            <div id="form-reg">
             
-               <form action="">
-                   <TextField id="outlined-basic" label="Nome" variant="outlined" margin="dense" size="small" sx={{
+                <form onSubmit={(event: React.ChangeEvent<HTMLFormElement>) => onSubimit(event)}>
+                   <TextField id="outlined-basic" label="Nome" value={nome ?? ""} onChange={(e) => setNome(e.target.value)} variant="outlined" margin="dense" size="small" sx={{
                     width: "100%",
                    }}/>
-                   <TextField id="outlined-basic" label="CNPJ" variant="outlined" margin="dense" size="small" sx={{
+                   <TextField id="outlined-basic" label="CNPJ" value={cnpj ?? ""} onChange={(e) => setCnpj(e.target.value)} variant="outlined" margin="dense" size="small" sx={{
                     width: "100%",
                    }}/>
-                   <TextField id="outlined-basic" label="Email" variant="outlined" margin="dense" size="small" sx={{
+                   <TextField id="outlined-basic" label="Email" value={email ?? ""} onChange={(e) => setEmail(e.target.value)} variant="outlined" margin="dense" size="small" sx={{
                     width: "100%",
                    }}/>
-                   <TextField id="outlined-basic" label="Senha" variant="outlined" margin="dense" size="small" sx={{
+                   <TextField id="outlined-basic" label="Senha" value={senha ?? ""} onChange={(e) => setSenha(e.target.value)} variant="outlined" margin="dense" size="small" sx={{
                     width: "100%",
                    }}/>
                    
+                    <Button 
+                        variant="contained"
+                        type="submit" 
+                        color='inherit' 
+                        sx={{ width: "100%" }} 
+                    >
+                        Realizar Cadastro
+                    </Button>
                </form>
                
            </div>
-           <Button variant="contained" color='inherit' href="#contained-buttons" sx={{
-            width: "100%",
-           }} onClick={handleClick}>
-           Realizar Cadastro
-           </Button>
             
-                
-                  
             </div>
 
         </div>
